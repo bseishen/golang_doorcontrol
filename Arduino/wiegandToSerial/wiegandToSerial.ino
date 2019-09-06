17e56ae47eafbab6d44b309b33a7bcf448b25959
Ben Eishen <ben@preteckt.com>
2019-09-06 13:49:19 -0500

    Using different wiegand library now, seems much more stable with various keypads

--- Arduino/wiegandToSerial/wiegandToSerial.ino
+++ Arduino/wiegandToSerial/wiegandToSerial.ino
@@ -5,25 +5,39 @@
  *
  * Usage:
  * UART is 9600N1
- * RFID badge scan will send an R folowed by the ASCII conversion of the number ending in a newline ie "R1234567\n"
+ * RFID badge scan will send an R(for 34bit wiegand) or S(for 26bit wiegand) folowed by the ASCII conversion of the number ending in a newline ie "R1234567\n"
  * Button presses will be sent over as they are pressed in ASCII
- * Enter and escape will send \n and \esc\n respectively.
+ * Enter(or and escape will send \n and \esc\n respectively.
  *
  * If an 'O' is received, it will toggle the door relay, key buzzer and led for 3 seconds and release.
  *
  * If an 'E' is received, it will rapidly flash the the led 5 times indicating there was an error.
+ *  
+ * If an 'B' is received, ring the bell two short times
+ * 
+ * This project uses a wiegand library located at https://github.com/paulo-raca/YetAnotherArduinoWiegandLibrary
+ * and has been tested with version 2.0
+ * 
+ * 
  */
+#include <Wiegand.h>
+
+//#define DEBUG 1
 
 #define DOOR_RELAY 7
 #define KEY_BUZZER 8
 #define KEY_LED    5
 #define BELL       6
-#define D0         2
-#define D1         3
+#define PIN_D0     2
+#define PIN_D1     3
 
-#include <Wiegand.h>
+//WIGAND Values for enter and esc (4bit)
+#define ESC_BUTTON    0x0A //esc
+#define ENTER_BUTTON  0x0B //enter
 
-WIEGAND wg;
+
+// The object that handles the wiegand protocol
+Wiegand wiegand;
 char inbyte;
 byte escCount; //Pressed 3 times, ring the bell
 
@@ -63,6 +77,7 @@ void ringBell(){
   }
 }
 
+// Initialize Wiegand reader
 void setup() {
   pinMode(DOOR_RELAY, OUTPUT);
   pinMode(KEY_BUZZER, OUTPUT);
@@ -73,49 +88,28 @@ void setup() {
   digitalWrite(KEY_BUZZER, HIGH);
   digitalWrite(KEY_LED,    HIGH);
   digitalWrite(BELL,       HIGH);
+  
+  Serial.begin(9600);
 
-	Serial.begin(9600);
-	wg.begin(D0, 0, D1, 1);
+  //Install listeners and initialize Wiegand reader
+  wiegand.onReceive(receivedData, "Card readed: ");
+  wiegand.onReceiveError(receivedDataError, "Card read error: ");
+  wiegand.onStateChange(stateChanged, "State changed: ");
+  wiegand.begin(Wiegand::LENGTH_ANY, true);
+
+  //initialize pins as INPUT
+  pinMode(PIN_D0, INPUT);
+  pinMode(PIN_D1, INPUT);
 }
 
+// Continuously checks for pending messages and polls updates from the wiegand inputs
 void loop() {
+  // Checks for pending messages
+  wiegand.flush();
 
-  //Wiegand Check and Send
-  if(wg.available()){
-
-    //Check for rfid tag data, if there is data preface it with 'R'
-    if((wg.getWiegandType()==26)||(wg.getWiegandType()==34)){
-      Serial.print("R");
-      Serial.print(wg.getCode());
-      Serial.print("\n");
-      escCount = 0;
-
-    //Keypresses are going to be 4 bit (usually).
-    }else{
-      switch(wg.getCode()){
-        //ESC
-        case 0x1B:
-          Serial.write(wg.getCode());
-          Serial.print("\n");
-          escCount++;
-          break;
-        //Enter
-        case 0x0D:
-          Serial.print("\n");
-          escCount = 0;
-          break;
-        //All other numbers
-        default:
-          Serial.print(wg.getCode());
-          escCount = 0;
-      }
-    }
-
-    if(escCount > 2){
-      escCount = 0;
-      ringBell();
-    }
-  }
+  // Check for changes on the the wiegand input pins
+  wiegand.setPin0State(digitalRead(PIN_D0));
+  wiegand.setPin1State(digitalRead(PIN_D1));
 
   if(Serial.available()){
     inbyte = Serial.read();
@@ -129,4 +123,97 @@ void loop() {
       ringBell();
     }
   }
+}
+
+// Notifies when a reader has been connected or disconnected.
+// Instead of a message, the seconds parameter can be anything you want -- Whatever you specify on `wiegand.onStateChange()`
+void stateChanged(bool plugged, const char* message) {
+#ifdef DEBUG
+    Serial.print(message);
+    Serial.println(plugged ? "CONNECTED" : "DISCONNECTED");
+#endif
+}
+
+// Notifies when a card was read.
+// Instead of a message, the seconds parameter can be anything you want -- Whatever you specify on `wiegand.onReceive()`
+void receivedData(uint8_t* data, uint8_t bits, const char* message) {
+    uint32_t t;
+
+    //Check for rfid tag data, if there is data preface it with 'R' if its wiegand 34
+    if(bits==32){
+      Serial.print("R");
+      t = ((uint32_t)data[3]&0xFF) | (((uint32_t)data[2]&0xFF) << 8) | (((uint32_t)data[1]&0xFF) << 16) | (((uint32_t)data[0]& 0xFF )<< 24);
+      Serial.print(t);
+      Serial.print("\n");
+      escCount = 0;
+    }
+    //Check for rfid tag data, if there is data preface it with 'S' if its wiegand 26
+    else if(bits==24){
+      Serial.print("S");
+      t = ((uint32_t)data[2]&0xFF) | (((uint32_t)data[1]&0xFF) << 8) | (((uint32_t)data[0]&0xFF) << 16);
+      Serial.print(t);
+      Serial.print("\n");
+      escCount = 0;
+    }
+
+    //Keypresses are going to be 4 bit (usually).
+    else if(bits==4){
+      t = (data[0] & 0x0000000F);
+      switch(t){
+        //ESC
+        case ESC_BUTTON:
+          Serial.write(0x1B);
+          Serial.print("\n");
+          escCount++;
+          break;
+        //Enter
+        case ENTER_BUTTON:
+          Serial.print("\n");
+          escCount = 0;
+          break;
+        //All other numbers
+        default:
+          Serial.print(t);
+          escCount = 0;
+      }
+    }
+
+
+    if(escCount > 2){
+      escCount = 0;
+      ringBell();
+    }
+
+#ifdef DEBUG
+      Serial.print(message);
+      Serial.print(bits);
+      Serial.print("bits / ");
+      //Print value in HEX
+      uint8_t bytes = (bits+7)/8;
+      for (int i=0; i<bytes; i++) {
+          Serial.print(data[i] >> 4, 16);
+          Serial.print(data[i] & 0xF, 16);
+      }
+      Serial.println();
+#endif
+   
+}
+
+// Notifies when an invalid transmission is detected
+void receivedDataError(Wiegand::DataError error, uint8_t* rawData, uint8_t rawBits, const char* message) {
+#ifdef DEBUG
+    Serial.print(message);
+    Serial.print(Wiegand::DataErrorStr(error));
+    Serial.print(" - Raw data: ");
+    Serial.print(rawBits);
+    Serial.print("bits / ");
+
+    //Print value in HEX
+    uint8_t bytes = (rawBits+7)/8;
+    for (int i=0; i<bytes; i++) {
+        Serial.print(rawData[i] >> 4, 16);
+        Serial.print(rawData[i] & 0xF, 16);
+    }
+    Serial.println();
+#endif
 }
